a65b539dc123ca2d925f5ce3e24327672ca702c5
lufebraz <luizbraz98@gmail.com>
2021-04-21 13:24:48 -0300

    2nd day

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+
+import format from 'date-fns/format';
+
+export function Header() {
+const currentDate = format(new Date(), 'EEE, MMMM d');
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="logo"/>
+
+      <p>The best to listen, always!</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+}
