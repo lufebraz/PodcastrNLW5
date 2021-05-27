948b90c6f87db5dbad4bf1bb176a0b43721f5978
lufebraz <luizbraz98@gmail.com>
2021-05-26 22:42:06 -0300

    deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/Document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
 
           <link rel="shortcut icon" href="/faviconpodcastr.png" type="image/x-icon"/>
         </Head>
